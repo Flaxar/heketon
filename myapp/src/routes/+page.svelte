d1970e1c623de9aa1b7adb1bb221a06c2f55fbd6
Flaxar <filipschwarz007@gmail.com>
2023-05-16 20:24:54 +0200

    dashboard basics

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -15,6 +15,13 @@
     }
 </style>
 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    function login() {
+        location.replace("/dashboard");
+    }
+</script>
+
 <div class="brno-background" style="background-image: url('/brno-background2.jpg');">
     <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center" style="opacity: 100%;">
         <div class="card">
@@ -35,7 +42,7 @@
                         <input type="password" class="form-control" id="passwordInput">
                     </div>
                     <div class="d-flex flex-column justify-content-center align-items-center">
-                        <button type="submit" class="btn btn-primary">Přihlásit se</button>
+                        <button type="submit" class="btn btn-primary" on:click={login}>Přihlásit se</button>
                     </div>
                 </form>
             </div>
---
+++ myapp/src/routes/dashboard/+page.svelte
@@ -0,0 +1,47 @@
+<style>
+    @import "/custom.scss";
+
+    .primary {
+        background-color: #62b6cb;
+    }
+
+    .secondary {
+        background-color: #bee9e8;
+    }
+
+    .btn-primary {
+        background-color: grey;
+        color: black;
+    }
+</style>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    function getName() {
+        // TODO: from api
+        return "Blakub Jaha"
+    }
+</script>
+
+<nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid primary">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color:black;">{getName()}</a>
+                </li>
+            </ul>   -->
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                     <a class="nav-link active" style="color:black;">Domovská stránka</a>
+                </li>
+            </ul>        
+        </div>
+
+        
+        
+      
+        <img src="/icon.png" style="width: 100px">
+      
+      
+    </div>
+</nav>
