ec46e4b7c64c7a19a52d31e0ffb45a20f6a94889
Wanzaz <ondra.pazourek@gmail.com>
2023-05-17 01:50:28 +0200

    Add red LED bulb feature

--- measurements/temperature.py
+++ measurements/temperature.py
@@ -1,6 +1,11 @@
 import bme280
 import smbus2
 from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
 
 port = 1
 address = 0x76 # Adafruit BME280 address. Other BME280s may be different
@@ -9,9 +14,14 @@ bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus,address)
 
 while True:
-    bme280_data = bme280.sample(bus,address)
-    humidity  = bme280_data.humidity
-    pressure  = bme280_data.pressure
-    ambient_temperature = bme280_data.temperature
-    print(ambient_temperature)
-    sleep(1)
+        bme280_data = bme280.sample(bus,address)
+        humidity = bme280_data.humidity
+        pressure = bme280_data.pressure
+        ambient_temperature = bme280_data.temperature
+        if ambient_temperature > 27:
+                GPIO.output(18, GPIO.HIGH)
+        else:
+                GPIO.output(18, GPIO.LOW)
+
+        print(ambient_temperature)
+        sleep(1)
