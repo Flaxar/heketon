89f4c712e68f94c50c905d659195957a2a943495
Flaxar <filipschwarz007@gmail.com>
2023-05-16 19:11:15 +0200

    login screen

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,6 +1,45 @@
-<script>
-    
-</script>
+<style>
+    @import "/custom.scss";
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    .primary {
+        background-color: #bee9e8;
+    }
+
+    .secondary {
+        background-color: #62b6cb;
+    }
+
+    .btn-primary {
+        background-color: grey;
+        color: black;
+    }
+</style>
+
+<div class="brno-background" style="background-image: url('/brno-background2.jpg');">
+    <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center" style="opacity: 100%;">
+        <div class="card">
+            <div class="container justify-content-center pt-3 primary">
+                <div class="d-flex flex-column justify-content-center align-items-center">
+                    <h2 class="justify-content-center">Přihlášení</h2>
+                </div>
+                
+            </div>
+            <div class="card-body secondary">
+                <form>
+                    <div class="mb-3">
+                        <label for="usernameInput" class="form-label">Uživatelské jméno</label>
+                        <input type="email" class="form-control" id="usernameInput" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label">Heslo</label>
+                        <input type="password" class="form-control" id="passwordInput">
+                    </div>
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                        <button type="submit" class="btn btn-primary">Přihlásit se</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
