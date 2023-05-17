7d7fa9ced68932241b17fbc46d8f1217ab6e0257
Wanzaz <ondra.pazourek@gmail.com>
2023-05-17 02:08:38 +0200

    Add blue LED light bulb

--- measurements/temperature.py
+++ measurements/temperature.py
@@ -6,6 +6,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 port = 1
 address = 0x76 # Adafruit BME280 address. Other BME280s may be different
@@ -17,11 +18,15 @@ while True:
         bme280_data = bme280.sample(bus,address)
         humidity = bme280_data.humidity
         pressure = bme280_data.pressure
-        ambient_temperature = bme280_data.temperature
-        if ambient_temperature > 27:
+        temperature = bme280_data.temperature
+        if temperature > 27:
+                GPIO.output(12, GPIO.HIGH)
+        elif temperature < 26:
+                GPIO.output(12, GPIO.LOW)
                 GPIO.output(18, GPIO.HIGH)
         else:
+                GPIO.output(12, GPIO.LOW)
                 GPIO.output(18, GPIO.LOW)
 
-        print(ambient_temperature)
+        print(temperature)
         sleep(1)
