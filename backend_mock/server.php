da76d429a2fba020cdf55dd7bebfe0bbcb1bd2ff
TENMAJKL <silder.hologram@gmail.com>
2023-05-17 09:45:06 +0200

    mock

---
+++ backend_mock/server.php
@@ -0,0 +1,25 @@
+<?php
+
+use Workerman\Worker;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$worker = new Worker('websocket://localhost:8000');
+
+$id = 0;
+
+$worker->onConnect = function ($connection) use (&$id) {
+    $connection->id = $id++;
+};
+
+$worker->onMessage = function ($connection, $data) {
+    $message = json_decode($data, true);
+    if ($message['from'] === 'hardware') {
+        foreach ($connection->worker->connections as $clientConnection) {
+            if ($clientConnection->id !== $connection->id) {
+                $clientConnection->send($message['data']);
+            }
+        }
+        return;
+    }
+};
---
+++ backend_mock/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '33db225ace8f526c08ef0b6fb10f2fb9c4d27eb0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '33db225ace8f526c08ef0b6fb10f2fb9c4d27eb0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'workerman/workerman' => array(
+            'pretty_version' => 'v4.1.10',
+            'version' => '4.1.10.0',
+            'reference' => 'e967b79f95b9251a72acb971be05623ec1a51e83',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../workerman/workerman',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
